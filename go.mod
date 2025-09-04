2de32fc62806cf20e469c8ac6a1f39519099d349
Jille Timmermans <jille@quis.cx>
2025-09-04 10:36:36 +0200

    Removed the wiki support, now just outputs json

--- go.mod
+++ go.mod
@@ -2,16 +2,6 @@ module github.com/Jille/spelslot-wiki-sync
 
 go 1.22.5
 
-require (
-	cgt.name/pkg/go-mwclient v1.3.0
-	github.com/samber/lo v1.47.0
-	github.com/sebdah/goldie/v2 v2.5.5
-)
+require github.com/samber/lo v1.47.0
 
-require (
-	github.com/antonholmquist/jason v1.0.0 // indirect
-	github.com/mrjones/oauth v0.0.0-20190623134757-126b35219450 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/sergi/go-diff v1.0.0 // indirect
-	golang.org/x/text v0.16.0 // indirect
-)
+require golang.org/x/text v0.16.0 // indirect
--- sync.go
+++ sync.go
@@ -1,90 +1,71 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
-	"fmt"
 	"log"
 	"os"
-	"regexp"
+	"strconv"
+	"strings"
 
-	"cgt.name/pkg/go-mwclient"
-	"cgt.name/pkg/go-mwclient/params"
 	"github.com/samber/lo"
-	"golang.org/x/text/unicode/norm"
 )
 
 var (
-	character     = flag.Int("character", 131438467, "First character to sync")
-	campaign      = flag.Bool("campaign", false, "Also sync all other characters in the campaign")
+	characters    = flag.String("characters", "131438467", "Comma separated list of characters to sync")
+	campaign      = flag.Bool("campaign", false, "Also sync all other characters in their campaigns")
 	cobaltSession = flag.String("cobalt-session", "", "Cobalt session ID")
-	wikiUser      = flag.String("wiki-user", "Spelbot", "Wiki username")
-	wikiPass      = flag.String("wiki-pass", "", "Wiki password")
-
-	characterTemplateRe = regexp.MustCompile(`(?ms)\n*^{{Character.*?}}\n*`)
 )
 
 func main() {
 	flag.Parse()
-	w := lo.Must(mwclient.New("https://spelslot.nl/codex/api.php", "Spelbot"))
-	lo.Must0(w.Login(*wikiUser, *wikiPass))
 
 	accessToken := lo.Must(getDDBAccessToken(*cobaltSession))
 
-	mainCharacter := lo.Must(fetchCharacter(accessToken, *character))
+	var todo []int
+	for _, n := range strings.Split(*characters, ",") {
+		id, err := strconv.Atoi(n)
+		if err != nil {
+			log.Fatalf("--characters got invalid %q: %v", n, err)
+		}
+		todo = append(todo, id)
+	}
 
-	if err := syncCharacter(w, mainCharacter); err != nil {
-		log.Printf("Failed to sync main character: %v", err)
+	ret := map[int]CharacterInfo{}
+
+	ok := true
+	for len(todo) > 0 {
+		id := todo[len(todo)-1]
+		todo = todo[:len(todo)-1]
+
+		if _, seen := ret[id]; seen {
+			continue
+		}
+
+		ch, err := fetchCharacter(accessToken, id)
+		if err != nil {
+			log.Printf("Failed to fetch %d: %v", id, err)
+			ok = false
+			continue
+		}
+		if *campaign {
+			for _, c := range ch.Data.Campaign.Characters {
+				todo = append(todo, c.CharacterID)
+			}
+		}
+
+		ret[id] = ch.Data
+	}
+
+	b, err := json.Marshal(ret)
+	if err != nil {
+		log.Fatalf("json.Marshal: %v", err)
+	}
+	if err := os.WriteFile("characters.json", b, 0644); err != nil {
+		log.Fatalf("os.WriteFile: %v", err)
+	}
+
+	if !ok {
 		os.Exit(1)
 	}
-
-	if *campaign {
-		ok := true
-		for _, c := range mainCharacter.Data.Campaign.Characters {
-			if c.CharacterID == mainCharacter.Data.ID {
-				continue
-			}
-			ch, err := fetchCharacter(accessToken, c.CharacterID)
-			if err != nil {
-				log.Printf("Failed to fetch %d (%s by %s): %v", c.CharacterID, c.CharacterName, c.Username, err)
-				ok = false
-				continue
-			}
-			if err := syncCharacter(w, ch); err != nil {
-				log.Printf("Failed to sync %d (%s by %s): %v", c.CharacterID, c.CharacterName, c.Username, err)
-				ok = false
-			}
-		}
-		if !ok {
-			os.Exit(1)
-		}
-	}
-}
-
-func syncCharacter(w *mwclient.Client, ch CharacterResponse) error {
-	text := characterToWikiPage(ch)
-
-	if err := w.Edit(params.Values{
-		"title":   "Character:" + norm.NFC.String(ch.Data.Name),
-		"text":    norm.NFC.String(text),
-		"summary": "Sync character from D&D Beyond",
-		"bot":     "",
-	}); err != nil && err != mwclient.ErrEditNoChange {
-		return err
-	}
-
-	csrfToken, err := w.GetToken(mwclient.CSRFToken)
-	if err != nil {
-		panic(fmt.Errorf("unable to obtain csrf token: %w", err))
-	}
-
-	if _, err := w.Post(params.Values{
-		"action":      "protect",
-		"title":       "Character:" + norm.NFC.String(ch.Data.Name),
-		"protections": "edit=sysop|move=sysop",
-		"reason":      "Any changes would be overwritten by Spelbot",
-		"token":       csrfToken,
-	}); err != nil {
-		return err
-	}
-	return nil
 }
